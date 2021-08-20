ad8a57a7888493246e8041abac852c717f482e17
Dhomini Fael <dhominisalgado@gmail.com>
2021-08-19 22:05:04 -0300

    criando usuario

--- project/account/admin.py
+++ project/account/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
-
 from .forms import UserChangeForm, UserCreationForm
 from .models import User
 
@@ -11,5 +10,5 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserCreationForm
     model = User
     fieldsets = auth_admin.UserAdmin.fieldsets + (
-        ("Campos adicionais", {"fields": ("idade", "foto", "telefone_pessoal", "telefone_comercial", "cpf", "data_contratacao", "demitido", "ultima_mudanca")}),
+        ("Campos adicionais", {"fields": ("idade", "foto", "telefone_pessoal", "telefone_comercial", "cpf",)}),
     )
--- project/account/models.py
+++ project/account/models.py
@@ -4,17 +4,14 @@ from django.utils.safestring import mark_safe
 from django.utils import timezone
 
 class User(AbstractUser):
-    idade = PositiveIntegerField(blank=True)
-    foto = ImageField('Foto de perfil', upload_to='images/%Y/%m/%d/%M/%f', blank=True, default='images/default.jpg')
-    telefone_pessoal = CharField('Telefone pessoal', max_length=20, blank=True)
-    telefone_comercial = CharField('Telefone de trabalho', max_length=25, blank=True)
-    cpf = CharField('CPF', max_length=25, blank=True)
-    data_contratacao = DateField('Data de contratação', default=timezone.now, blank=True)
-    demitido = BooleanField(default=False, blank=True)
-    ultima_mudanca = DateField('Última mudança', auto_now=True, blank=True)
+    idade = PositiveIntegerField(blank=True, null=True)
+    foto = ImageField('Foto de perfil', upload_to='images/%Y/%m/%d/%M/%f', blank=True, default='images/default.jpg', null=True)
+    telefone_pessoal = CharField('Telefone pessoal', max_length=20, blank=True, null=True)
+    telefone_comercial = CharField('Telefone de trabalho', max_length=25, blank=True, null=True)
+    cpf = CharField('CPF', max_length=25, blank=True, null=True)
     
     def __str__(self):
-        return self.nome
+        return self.username
     
     @mark_safe
     def icon(self):
