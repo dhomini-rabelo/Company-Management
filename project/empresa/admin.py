1528f1ef955b336bd82c53398d857502d3e3c385
Dhomini Fael <dhominisalgado@gmail.com>
2021-08-22 03:09:59 -0300

    Novos models e esboço de tela

--- project/empresa/admin.py
+++ project/empresa/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Funcionario
+from .models import *
+
 
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
-    list_display = 'icone', 'nome', 'telefone_comercial', 'salario', 'admin'
+    list_display = 'icone', 'nome', 'telefone_comercial', 'salario'
     list_display_links = 'nome',
     list_filter = 'demitido',
+
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = 'logo', 'nome', 'presidente'
+    list_display_links = 'nome',
--- project/empresa/models.py
+++ project/empresa/models.py
@@ -4,6 +4,8 @@ from django.utils.safestring import mark_safe
 from django.utils import timezone
 from account.models import User
 from django.db import models
+from decimal import Decimal
+
 
 class Funcionario(Model):
     nome = CharField(max_length=120)
@@ -13,60 +15,85 @@ class Funcionario(Model):
     telefone_pessoal = CharField('Telefone pessoal', max_length=20, blank=True)
     telefone_comercial = CharField('Telefone de trabalho', max_length=25, blank=True)
     cpf = CharField('CPF', max_length=25, unique=True, blank=True)
-    admin = ForeignKey(User, on_delete=CASCADE)
     data_registro = DateField('Data de registro', auto_now_add=True)
     demitido = BooleanField(default=False)
     ultima_mudanca = DateField('Última mudança', auto_now=True)
-    
+    profissao = CharField(max_length=120, default='desempregado')
+
     def __str__(self):
         return self.nome
     
     @mark_safe
     def icone(self):
         return f'<a href="/media/{self.foto}" target="_blank"><img src="/media/{self.foto}" style="width: 35px; height: 25px;"></a>'
+
     
-    
+class Despesa(Model):
+    nome = CharField(max_length=120)
+    descricao = TextField('Descrição', blank=True) 
+    valor = DecimalField(max_digits=12, decimal_places=2, default=Decimal('0.00'), blank=True)
+
+
 class Empresa(Model):
     nome = CharField(max_length=120)
-    logo = ImageField(upload_to='images/company/%Y/%m/%d/%M/%f', blank=True)
-    foto = ImageField(upload_to='images/company/%Y/%m/%d/%M/%f', blank=True)
+    logo = ImageField(upload_to='images/company/%Y/%m/%d/%M/%f', default='images/logo.jpg', blank=True)
+    foto = ImageField(upload_to='images/company/%Y/%m/%d/%M/%f', default='images/empresa.jpg', blank=True)
     descricao = TextField('Descrição', blank=True) 
-    presidente = ForeignKey(User, on_delete=CASCADE)
-    funcionarios = ManyToManyField(Funcionario, verbose_name='Funcionários')
-    despesas = DecimalField('Despesas Mensais', max_digits=12, decimal_places=2)
+    presidente = ForeignKey(User, on_delete=DO_NOTHING)
+    funcionarios = ManyToManyField(Funcionario, verbose_name='Funcionários', blank=True)
+    despesas = ManyToManyField(Despesa, blank=True) 
     data_de_criacao = DateField('Data de criação', blank=True)
     fundador = CharField(max_length=120, blank=True)
+    valor = DecimalField(max_digits=12, decimal_places=2, default=Decimal('0.00'), blank=True)
+    
+    def __str__(self):
+        return self.nome
+    
+    @mark_safe
+    def logo(self):
+        return f'<a href="/media/{self.logo}" target="_blank"><img src="/media/{self.logo}" style="width: 35px; height: 25px;"></a>'
+    
+    @mark_safe
+    def foto(self):
+        return f'<a href="/media/{self.foto}" target="_blank"><img src="/media/{self.foto}" style="width: 35px; height: 25px;"></a>'
     
     
-class Setor(Model):
-    nome = CharField(max_length=120)
-    empresa = ForeignKey(Empresa, on_delete=CASCADE)
-    funcao = TextField('Função', blank=True) 
-    funcionarios = ManyToManyField(Funcionario, verbose_name='Funcionários')
-    descricao = TextField('Descrição', blank=True) 
+# class Setor(Model):
+#     nome = CharField(max_length=120)
+#     empresa = ForeignKey(Empresa, on_delete=CASCADE)
+#     funcao = TextField('Função', blank=True) 
+#     funcionarios = ManyToManyField(Funcionario, verbose_name='Funcionários')
+#     descricao = TextField('Descrição', blank=True) 
+#     tarefas = ManyToManyField(Tarefa)
     
     
-class Tarefa(Model):
-    nome = CharField(max_length=120)
-    descricao = TextField('Descrição', blank=True) 
-    finalizado = BooleanField(default=False)
-    fases = PositiveIntegerField(blank=True)
+# class Tarefa(Model):
+#     nome = CharField(max_length=120)
+#     descricao = TextField('Descrição', blank=True) 
+#     finalizado = BooleanField(default=False)
+#     fases = PositiveIntegerField(blank=True)
+#     gastos = DecimalField(max_digits=12, decimal_places=2)
     
     
-class Etapa(Model):
-    nome = CharField(max_length=120)
-    descricao = TextField('Descrição', blank=True) 
-    finalizado = BooleanField(default=False)
-    tarefas = ManyToManyField(Tarefa)
+# class Etapa(Model):
+#     nome = CharField(max_length=120)
+#     descricao = TextField('Descrição', blank=True) 
+#     finalizado = BooleanField(default=False)
+#     tarefas = ManyToManyField(Tarefa)
 
 
-class Projeto(Model):
-    imagem_principal = ImageField(upload_to='images/project/%Y/%m/%d/%M/%f')
-    titulo = CharField(max_length=120)
-    descricao = TextField('Descrição') 
-    gastos = DecimalField(max_digits=12, decimal_places=2)
-    lucros = DecimalField(max_digits=12, decimal_places=2)
-    empresa = ForeignKey(Empresa, on_delete=CASCADE)
-    etapas = ManyToManyField(Etapa)
-    funcionarios = ManyToManyField(Funcionario, verbose_name='Funcionários')
+# class Projeto(Model):
+#     imagem = ImageField(upload_to='images/project/%Y/%m/%d/%M/%f')
+#     titulo = CharField(max_length=120)
+#     descricao = TextField('Descrição') 
+#     gastos = DecimalField(max_digits=12, decimal_places=2)
+#     lucros = DecimalField(max_digits=12, decimal_places=2)
+#     empresa = ForeignKey(Empresa, on_delete=CASCADE)
+#     etapas = ManyToManyField(Etapa)
+#     funcionarios = ManyToManyField(Funcionario, verbose_name='Funcionários')
+#foto001 opcional
+#foto001
+#foto001
+#foto001
+#foto001
     
