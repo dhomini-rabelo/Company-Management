ded0e3e8fa5f540c96481b00d4452a71fad89119
Dhomini Fael <dhominisalgado@gmail.com>
2021-11-15 14:28:21 -0300

    tentativa deploy 25

--- project/COMPANY/wsgi.py
+++ project/COMPANY/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.COMPANY.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'COMPANY.settings')
 
 application = get_wsgi_application()
