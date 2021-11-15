143d5a93b089e205a4694acf83356c0a57aae64a
Dhomini Fael <dhominisalgado@gmail.com>
2021-11-15 11:02:53 -0300

    melhorando installed apps-2

--- project/COMPANY/settings.py
+++ project/COMPANY/settings.py
@@ -22,8 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account.apps.AccountConfig',
-    'empresa.apps.EmpresaConfig',
+    'project.account.apps.AccountConfig',
+    'project.empresa.apps.EmpresaConfig',
     'crispy_forms',
 ]
 
