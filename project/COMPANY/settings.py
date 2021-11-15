078450ae49486b762473c32acc45aefa8a7523c4
Dhomini Fael <dhominisalgado@gmail.com>
2021-11-15 11:43:14 -0300

    tentativa deploy 10

--- project/COMPANY/settings.py
+++ project/COMPANY/settings.py
@@ -23,8 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'account.apps.AccountConfig',
-    'empresa.apps.EmpresaConfig',
+    'account',
+    'empresa',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
